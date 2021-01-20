fd65e9cacae77536dbe0885b788068255e972dc3
hesham <hesham@heshams-iMac.local>
2021-01-20 16:38:41 +0300

    yay product added

---
+++ src/products.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: "Black coffee",
+    price: "1 BD",
+    image:
+      "https://images.deliveryhero.io/image/talabat/MenuItems/hot_coffee_637219611369583967.jpg",
+  },
+  {
+    name: "Vanila Ice shake",
+    price: "1.5 BD",
+    image:
+      "https://images.deliveryhero.io/image/talabat/MenuItems/vanila_ice_shaking_637033372911471567.jpg",
+  },
+];
+
+export default products;
